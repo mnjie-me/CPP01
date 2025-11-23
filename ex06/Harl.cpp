bcf7720bd022b65cb52dfcc2c59c91d41afab3fd
Mari Cruz <maricruz.njie@gmail.com>
2025-11-23 11:19:27 +0100

    create harl filter

---
+++ ex06/Harl.cpp
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mari-cruz <mari-cruz@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/22 11:43:51 by mari-cruz         #+#    #+#             */
+/*   Updated: 2025/11/23 11:18:28 by mari-cruz        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+void Harl::debug(void)
+{
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon."
+	<< std::endl << std::endl;
+}
+
+void Harl::info(void)
+{
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money."
+	<< std::endl << std::endl;
+}
+
+void Harl::warning(void)
+{
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free."
+	<< std::endl << std::endl;
+}
+
+void Harl::error(void)
+{
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now."
+	<< std::endl << std::endl;
+}
+
+size_t Harl::getIndex(std::string level)
+{
+	std::string levels[] = {"debug", "info", "warning", "error"};
+	
+	for (size_t i = 0; i < 4; i++)
+	{
+		if (levels[i] == level)
+			return (i);
+	}
+	return (-1);
+}
+
+void Harl::complain(std::string level)
+{
+	size_t index = getIndex(level);
+	
+	switch (index)
+	{
+		case 0:
+			debug();
+			info();
+			warning();
+			error();
+			break ;
+		case 1:
+			info();
+			warning();
+			error();
+			break ;
+		case 2:
+			warning();
+			error();
+			break ;
+		case 3:
+			error();
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems]" << std::endl;
+	}
+}
